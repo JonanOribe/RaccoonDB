836a91333c3f9567c679e074605cb3d113e10294
Jon Ander Oribe <jonan.oribe@gmail.com>
2022-11-02 12:18:05 +0100

    Update main.go

--- main.go
+++ main.go
@@ -63,52 +63,53 @@ return &driver, os.MkdirAll(dir,0755)
 
 func (d *Driver) Write(collection, resource string,v interface{}) error{
 
-if collection == ""{
-	return fmt.Errorf("Missing collection - no place to save record!")
-}
-
-if resource == ""{
-	return fmt.Errorf("Missing resource - unable to save record (no name)!")
-}
-
-mutex:= g.getOrCreateMutex(collection)
-mutex.Lock()
-defer mutex.Unlock()
-
-dir := filepath.Join(d.dir,collection)
-fnlPath := filepath.Join(dir, resource+".json")
-tmpPath := fnlPath + ".tmp"
-
-if err := os.MkdirAll(dir,0755);err != nil{
-	return err
-}
-
-b, err := json.MarshalIndent(v, "", "\t")
-
-if err != nil{
-	return err
-}
-
-b = append(b,byte('\n'))
-
-if err := ioutil.WriteFile(tmpPath, b, 0644);err != nil{
-	return err
-}
-
-}
-
-func (d *Driver) Read(collection, resource string, v interface{}) error{
 	if collection == ""{
 		return fmt.Errorf("Missing collection - no place to save record!")
 	}
-	
+
 	if resource == ""{
 		return fmt.Errorf("Missing resource - unable to save record (no name)!")
 	}
 
+	mutex:= g.getOrCreateMutex(collection)
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	dir := filepath.Join(d.dir,collection)
+	fnlPath := filepath.Join(dir, resource+".json")
+	tmpPath := fnlPath + ".tmp"
+
+	if err := os.MkdirAll(dir,0755);err != nil{
+		return err
+	}
+
+	b, err := json.MarshalIndent(v, "", "\t")
+
+	if err != nil{
+		return err
+	}
+
+	b = append(b,byte('\n'))
+
+	if err := ioutil.WriteFile(tmpPath, b, 0644);err != nil{
+		return err
+	}
+
+	return os.Rename(tmpPath,fnlPath)
+}
+
+func (d *Driver) Read(collection, resource string, v interface{}) error{
+	if collection == ""{
+		return fmt.Errorf("Missing collection - unable to read!")
+	}
+	
+	if resource == ""{
+		return fmt.Errorf("Missing resource - unable to read record (no name)!")
+	}
+
 	record := filepath.Join(d.dir,collection, resource)
 
-	if _,err :=stat(record); err != nil{
+	if _,err := stat(record); err != nil{
 		return err
 	}
 
@@ -121,8 +122,30 @@ func (d *Driver) Read(collection, resource string, v interface{}) error{
 	return json.Unmarshal(b,&v)
 }
 
-func (d *Driver) ReadAll(){
+func (d *Driver) ReadAll(collection string)([]string, error){
+	if collection == "" {
+		return nil, fmt.Errorf("Missing collection - unable to read!")
+	}
+	dir := filepath.Join(d.dir,collection)
 
+	if _,err := stat(dir);err != nil{
+		return nil, err
+	}
+
+	files, _ := ioutil.ReadDir(dir)
+
+	var records []string
+
+	for _, file := range files{
+		b, err := iouioutil.ReadFile(filepath.Join(dir, file.Name()))
+		if err != nil{
+			return nil, err
+		}
+
+		records = append(records, string(b))
+	}
+
+	return records, nil
 }
 
 func (d *Driver) Delete() error{
