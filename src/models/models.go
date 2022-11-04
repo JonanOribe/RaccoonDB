7fd3291ae25df5c5e2c4d16596d99f240edad38e
Jon Ander Oribe <jonan.oribe@gmail.com>
2022-11-04 09:22:43 +0100

    Base

--- src/models/models.go
+++ src/models/models.go
@@ -4,6 +4,14 @@ import (
 	"encoding/json"
 )
 
+type User struct {
+	Name    string
+	Age     json.Number
+	Contact string
+	Company string
+	Address Address
+}
+
 type Address struct {
 	City    string
 	State   string
